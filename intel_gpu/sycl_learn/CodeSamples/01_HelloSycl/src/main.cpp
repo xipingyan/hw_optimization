3e718d4a17c919710322987834d2a7b2ecafe178
xipingya <xiping.yan@intel.com>
2024-10-30 08:37:51 +0000

    xx

--- intel_gpu/sycl_learn/CodeSamples/01_HelloSycl/src/main.cpp
+++ intel_gpu/sycl_learn/CodeSamples/01_HelloSycl/src/main.cpp
@@ -8,7 +8,7 @@
 std::vector<float> vec_add(std::vector<float>& a, std::vector<float>& b, sycl::queue& queue) {
     std::vector<float> sum(a.size());
     // Compute the first n_items values in a well known sequence
-    constexpr int n_items = a.size();
+    constexpr int n_items = 10000;
     float *items = sycl::malloc_shared<float>(n_items, queue);
     float *a_dev = sycl::malloc_shared<float>(n_items, queue);
     float *b_dev = sycl::malloc_shared<float>(n_items, queue);
@@ -17,16 +17,20 @@ std::vector<float> vec_add(std::vector<float>& a, std::vector<float>& b, sycl::q
         b_dev[i] = b[i];
     }
 
-    queue.parallel_for(sycl::range<4>(n_items), [items] (sycl::id<1> i) {
-        float x1 = powf((1.0f + sqrtf(5.0))/2, i);
-        float x2 = powf((1.0f - sqrtf(5.0))/2, i);
-        items[i] = roundf((x1 - x2)/sqrtf(5));
+    queue.parallel_for(sycl::range<1>(n_items), [items, a_dev, b_dev] (sycl::id<1> i) {
+        float x1 = a_dev[i];
+        float x2 = b_dev[i];
+        items[i] = roundf(x1+x2);
     }).wait();
 
-    for(int i = 0 ; i < n_items ; ++i) {
-        std::cout << items[i] << std::endl;
+    // for(int i = 0 ; i < n_items ; ++i) {
+    //     std::cout << items[i] << std::endl;
+    // }
+    for (size_t i = 0; i < 10000; i++) {
+            sum[i] = items[i];
     }
     free(items, queue);
+    return sum;
 }
 
 int main(int argc, char* argv[])
@@ -45,8 +49,8 @@ int main(int argc, char* argv[])
     auto result = vec_add(a, b, queue);
 
     for (int i = 0; i < 10000; i++) {
-        if (fabs(expected[i] - result[i]) > 0.001f) {
-            std::cout << "== Result [" << i << "] diff: " << fabs(expected[i] - result[i]) << std::endl;
+        if (fabs(expected[i] - result[i]) > 0.1f) {
+            std::cout << "== Result [" << i << "] diff: " << fabs(expected[i] - result[i]) << ", result=" << result[i] <<  std::endl;
         }
     }
 
