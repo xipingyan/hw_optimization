a9e259258447d2640fc695bed6cd74abe2384000
Yan <xiping.yan@intel.com>
2024-10-30 10:48:42 +0800

    Add first test of sycl

--- intel_gpu/sycl_learn/CodeSamples/01_HelloSycl/src/main.cpp
+++ intel_gpu/sycl_learn/CodeSamples/01_HelloSycl/src/main.cpp
@@ -1,8 +1,31 @@
-#include <iostream>
 // Reference:
 // https://www.intel.com/content/www/us/en/docs/dpcpp-cpp-compiler/developer-guide-reference/2024-1/use-cmake-with-the-compiler.html
 
-int main(int argc, char** argv) {
-    std::cout << "Hello SYCL." << std::endl;
+#include <iostream>
+#include <sycl/sycl.hpp>
+#include <cmath>
+
+int main(int argc, char* argv[])
+{
+    sycl::queue queue;
+
+    std::cout << "Using "
+        << queue.get_device().get_info<sycl::info::device::name>()
+        << std::endl;
+
+    // Compute the first n_items values in a well known sequence
+    constexpr int n_items = 16;
+    int *items = sycl::malloc_shared<int>(n_items, queue);
+    queue.parallel_for(sycl::range<1>(n_items), [items] (sycl::id<1> i) {
+        double x1 = pow((1.0 + sqrt(5.0))/2, i);
+        double x2 = pow((1.0 - sqrt(5.0))/2, i);
+        items[i] = round((x1 - x2)/sqrt(5));
+    }).wait();
+
+    for(int i = 0 ; i < n_items ; ++i) {
+        std::cout << items[i] << std::endl;
+    }
+    free(items, queue);
+
     return 0;
 }
